23c9de62c49de6107bea647f75a1f0d66157bf1f
Manuel Schmalstieg <git.code@ms-studio.net>
2016-12-06 14:45:23 +0100

    redirection vers la page d’inscription - fixes #129

--- page-inscription-kino.php
+++ page-inscription-kino.php
@@ -2,8 +2,9 @@
 
 /*
 
-- si connecté = redirige vers http://kinogeneva.ch/members/%username%/profile/edit/
-- si non-connecté = dirige vers la page http://kinogeneva.ch/inscription-membre/
+- template de redirection pour https://kinogeneva.ch/inscription-kino/
+- si connecté = redirige vers https://kinogeneva.ch/members/%username%/profile/edit/
+- si non-connecté = dirige vers la page https://kinogeneva.ch/inscription-membre/
 
 */
 
@@ -15,7 +16,9 @@ if ( is_user_logged_in() ) {
 
 } else {
 	
-	$kino_redirect =  site_url( 'wp-login.php?redirect_to=http%3A%2F%2Fkinogeneva.ch%2Finscription-kino%2F' );
+	// $kino_redirect =  site_url( 'wp-login.php?redirect_to=http%3A%2F%2Fkinogeneva.ch%2Finscription-kino%2F' );
+	$kino_redirect =  site_url( 'inscription-membre' );
+	  
 }
 
 header("Location: ".$kino_redirect); /* Redirect browser */
