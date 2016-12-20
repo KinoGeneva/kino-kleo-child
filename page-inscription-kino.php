b37df40f6e3c96c9443ce85799972ae753bfc744
Manuel Schmalstieg <git.code@ms-studio.net>
2016-12-20 23:52:01 +0100

    petits correctifs pour prévenir des bugs de redirection

--- page-inscription-kino.php
+++ page-inscription-kino.php
@@ -17,9 +17,30 @@ if ( is_user_logged_in() ) {
 } else {
 	
 	// $kino_redirect =  site_url( 'wp-login.php?redirect_to=http%3A%2F%2Fkinogeneva.ch%2Finscription-kino%2F' );
-	$kino_redirect =  site_url( 'inscription-membre' );
+	$kino_redirect =  home_url( 'inscription-membre' );
 	  
 }
 
-header("Location: ".$kino_redirect); /* Redirect browser */
-exit();
+// Note: with some plugins such as popup-by-supsystic, it's possible that headers are already sent
+// See http://stackoverflow.com/questions/2820639/how-to-check-if-headers-already-been-sent-in-php 
+// for some solutions...
+
+if ( headers_sent() ) { 
+	
+	// if headers already sent : print HTML redirect.
+	// echo '<h1>Headers already sent</h1>';
+	echo '<!DOCTYPE html>
+	<html class="no-js" lang="fr-FR">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="refresh" content="0; url='.home_url().'">
+	</head>
+	<body></body>
+	</html>';
+	
+} else {
+	//send the user automatically to test.php
+	header("Location: ".$kino_redirect);
+	exit;
+
+}
