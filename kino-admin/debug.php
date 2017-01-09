83929ab80c4e914044002631b2a8d80f7644a42d
Manuel Schmalstieg <git.code@ms-studio.net>
2017-01-09 23:09:38 +0100

    modif de la page debug pour analyser les emails

--- kino-admin/debug.php
+++ kino-admin/debug.php
@@ -30,38 +30,9 @@ if ( get_cfield( 'centered_text' ) == 1 )  {
         
         $kino_fields = kino_test_fields();
         
-        $kinoites_benevoles = array();
-        
-//        $ids_of_benevoles = get_objects_in_term( 
-//        	$kino_fields['group-benevoles-kabaret'] , 
-//        	'user-group' 
-//        );
-
-				$ids_of_kino_participants = get_objects_in_term( 
-					$kino_fields['group-kino-pending'] , 
-					'user-group' 
-				);
-        
-        $ids_of_kino_complete = get_objects_in_term( 
-        	$kino_fields['group-kino-complete'] , 
-        	'user-group' 
-        );
-        
-        $ids_of_kino_incomplete = get_objects_in_term( 
-        	$kino_fields['group-kino-incomplete'] , 
-        	'user-group' 
-        );
-        
-        $ids_real_kabaret_accepted = get_objects_in_term( 
-        	$kino_fields['group-real-kabaret'] , 
-        	'user-group' 
-        );
-
-//        
-//        $ids_real_kabaret_accepted = get_objects_in_term( 
-//        	$kino_fields['group-real-kabaret'] , 
-//        	'user-group' 
-//        );
+        $users_bp_email_missing = array();
+        $users_email_same = array();
+        $users_email_diff = array();
 
 				// enlever les champs zéro: 
 //				$ids_of_kino_complete = array_filter($ids_of_kino_complete);
@@ -72,7 +43,7 @@ if ( get_cfield( 'centered_text' ) == 1 )  {
         //***************************************
         
 	        $user_query = new WP_User_Query( array( 
-	        	 'include' => $ids_of_kino_participants, // IDs incluses
+	        	//  'include' => $ids_of_kino_participants, // IDs incluses
 	        	// 'include' => $ids_real_kabaret_accepted, 
 //	        	'orderby' => 'name',
 //	        	'order' => 'ASC' 
@@ -80,111 +51,110 @@ if ( get_cfield( 'centered_text' ) == 1 )  {
 	        	'order' => 'DESC'
 	        ) );
 	        
-	        echo '<p>Nombre total: '.count($user_query->results).'</p>';
+	        $total = count($user_query->results);
+	        
+	        echo '<p>Nombre total: '. $total .'</p>';
         
-        $metronom = 1;
-        
-        $users_in_kabaret = array();
-        $users_not_in_kabaret = array();
-        $users_nom_manquant = array();
+//        $users_in_kabaret = array();
+//        $users_not_in_kabaret = array();
+//        $users_nom_manquant = array();
         
         if ( ! empty( $user_query->results ) ) {
         	foreach ( $user_query->results as $user ) {
         		
         		// infos about WP_user object
-        		$userid = $user->ID ;
+        		$user_id = $user->ID ;
         		// echo $user->user_email.'</br>';
         		        		
-        		// étape 1: tester si le champ Participation Kino est coché!
-        		
-        		echo $user->user_email.'</br>';
-        		
         		// Participe au cabaret 2016??
+        		
         		$kino_test = bp_get_profile_field_data( array(
         				'field'   => $kino_fields['kabaret'], 
-        				'user_id' => $userid
+        				'user_id' => $user_id
         		) );
+        		
         		if ( ( $kino_test == "oui" ) || ( $kino_test == "yes" ) ) {
         					
-        					$users_in_kabaret[] = $userid;
+        					$participants_kino[] = $user_id;
         					
-        					// 
-        				
-        				        					
-//        					echo ' / '.$metronom++;
-//        					echo ' : '.$userid;
-//        					
-//        					if ( in_array( $userid, $ids_of_kino_participants ) ) {          				            				
-//        					  echo ' ok ';
-//        					} else {
-//        						echo ' PROBLEM! ';
-//        						
-//        						// add to group
-//        						 kino_add_to_usergroup( $userid, 
-//        						 	$kino_fields['group-kino-pending'] );
-//        					}
+        					// étape 1: tester si le champ Participation Kino est coché!
         					
-        					// Ajouter au groupe "group-kino-pending"
+        					$kino_wp_email = $user->user_email;
         					
-        					// Add to Mailpoet List
-//        					kino_add_to_mailpoet_list( $id, 
-//        						$kino_fields['mailpoet-real-platform'] );
+        					$kino_wp_email = strtolower( $kino_wp_email );
         					
-        					// Profil Cabaret 2016 complet? 
-        					
-        		} else {
-        			
-        			// is part of $ids_of_kino_participants? 
-        			
-//        			if ( in_array( $userid, $ids_of_kino_participants )) {
-//        				// echo ' / paradox: user '.$userid ;
-//        			}
-        			
-        			
-//        			echo ' / '.$metronom++;
-//        			echo ' user not : '.$userid;
-        			// echo ' '.$userid.' <br/>';
-        				
-        				$users_not_in_kabaret[] = $userid;
-        				
-        				// echo $user->user_email.'</br>';
-        			
-        		}
+			        		$kino_bp_email = bp_get_profile_field_data( array(
+			        				'field'   => $kino_fields['courriel'],
+			        				'user_id' => $user_id
+			        		) );
+			        		
+			        		// produit du HTML - <a href="mailto:yan.marquet80@gmail.com" rel="nofollow">yan.marquet80@gmail.com</a>
+			        		
+			        		$kino_bp_email = strip_tags( $kino_bp_email );
+			        		
+			        		$kino_bp_email = strtolower( $kino_bp_email );
+			        		
+			        		if ( empty( $kino_bp_email ) ) {
+			        							
+			        							echo '<a href="/members/'.$user->user_nicename.'/">'.$kino_wp_email.'</a> - BP Email missing</br>';
+			        							
+			        							$users_bp_email_missing[] = $user_id;
+			        							
+			        							// updating all email fields
+      												xprofile_set_field_data(  
+      													$kino_fields['courriel'],  
+      													$user_id, 
+      													$kino_wp_email
+      												);
+			        							
+									} else if ( strcmp ( $kino_bp_email, $kino_wp_email ) == 0  ) {
+									
+										// echo 'Email ('.$kino_wp_email.') identique</br>';
+										
+										$users_email_same[] = $user_id;
+										
+									} else {
+									
+										$users_email_diff[] = $user_id;
+										
+										echo 'Email différent:  WP: ';
+										echo '<a href="/members/'.$user->user_nicename.'/profile/">'.$kino_wp_email.'</a> ';
+										echo 'et BP: '.$kino_bp_email.'</br>';
+										
+//										 $update_result = wp_update_user( array( 
+//											'ID' => $user_id, 
+//											'user_email' => $kino_bp_email ) 
+//										);
+//										
+//										if ( is_wp_error( $update_result ) ) {
+//											echo 'There was an error with user '.$user_id.'.</br>';
+//											echo '<pre>';
+//											var_dump($update_result);
+//											echo '</pre>';
+//										} else {
+//											echo 'Success!</br>';
+//										}
+									
+									} // end email testing
         		
-        		// $kinoite_participation = kino_user_participation( $kino_userid, $kino_fields );
+        		} // end kabaret testing
+        		
         	
         	} // End foreach
         } // End testing user_query_cherche	
         
+        $participants_nr = count($participants_kino);
         
-        echo '<p>Users in kabaret: '.count($users_in_kabaret).'</p>';
+        echo '<p>Participants: '.$participants_nr.' / '. $total .' </p>';
         
-        echo '<p>Users not in kabaret: '.count($users_not_in_kabaret).'</p>';
+        echo '<p>Users avec mail BP vide: '.count($users_bp_email_missing).' / '. $participants_nr .' </p>';
         
-        echo '<p>Users au nom manquant: '.count($users_nom_manquant).'</p>';
+        echo '<p>Users avec mail similaire: '.count($users_email_same).' / '. $participants_nr .' </p>';
         
-//        echo '<pre> $users_nom_manquant: ';
-//        var_dump($users_nom_manquant);
-//        echo '</pre>';
+        echo '<p>Users avec mail différent: '.count($users_email_diff).' / '. $participants_nr .' </p>';
         
-        // Add to mailpoet list: Identity Name= id 21
-        
-//        kino_add_to_mailpoet_list_array( 
-//        	$users_nom_manquant, 
-//        	21 
-//        );
-        
-//        echo '<p>Users in group-kino-pending: '.count($ids_of_kino_participants).'</p>';
-        
-        // diff between them:
-//        $ids_paradox = array_diff( $ids_of_kino_participants, $users_in_kabaret );
-        
-//        echo '<pre> $ids_paradox: ';
-//        var_dump($ids_paradox);
-//        echo '</pre>';
-        
+  
         // ***********************************
-        
         
         ?>
         
