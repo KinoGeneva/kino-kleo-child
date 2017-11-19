71da0b3d9220e524b0fe4c61044f331d65354826
nat <nat@etisse.ch>
2017-11-19 17:12:17 +0100

    Ticket #209 vérifier que ACF est actif avant d'ajouter les fichiers liés à ACF

--- buddypress/groups/single/home.php
+++ buddypress/groups/single/home.php
@@ -88,10 +88,21 @@
 				if ( bp_group_is_visible() ) {
 					
 					//on active le template personnalisé
-					bp_get_template_part( 'groups/single/home-add');
-					
-					// Load appropriate front template
-					//bp_groups_front_template_part();
+					//d'abord tester que le plugin ACF est actif
+					/**
+					 * Detect plugin. For use on Front End only.
+					 */
+					include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+					// check for plugin using plugin name
+					if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
+					  //plugin is activated
+					  bp_get_template_part( 'groups/single/home-add');
+					}
+					else {
+						// Load appropriate front template
+						bp_groups_front_template_part();
+					}
 
 				} else {
 
