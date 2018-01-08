b3560803e5addb2d79f7f0144e9627d60b05eb45
Manuel Schmalstieg <git.code@ms-studio.net>
2018-01-08 22:19:03 +0100

    template "recherche de bénévoles"

---
+++ kino-admin/recherche-de-benevoles.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Un template pour valider les inscriptions
+ */
+?>
+
+<?php
+$extra_classes = 'clearfix';
+if ( get_cfield( 'centered_text' ) == 1 )  {
+    $extra_classes .= ' text-center';
+}
+?>
+
+<!-- Begin Article -->
+<article id="post-<?php the_ID(); ?>" <?php post_class($extra_classes); ?>>
+
+    <div class="article-content">
+        <?php the_content(); ?>
+           </div><!--end article-content-->
+  
+    <?php  ?>
+</article>
+<!-- End  Article -->
+<?php 
+
+?>
