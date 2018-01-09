1c9aa3828d8d47b27138386b17f2329b923968f3
Manuel Schmalstieg <git.code@ms-studio.net>
2018-01-09 09:06:21 +0100

    Améliorations profil membre, menu de 2eme niveau
    - pour meilleure organisation du code, le menu #subnav est mis dans un fichier php séparé.
    - ce menu n'est plus affiché pour visiteurs non connectés (il n'a aucune utilité, ne comportant alors que le bouton "afficher").

--- buddypress/members/single/profile.php
+++ buddypress/members/single/profile.php
@@ -7,40 +7,16 @@
  * @subpackage bp-legacy
  */
 
-?>
+// Navigation de 2ème niveau
+// Afficher seulement si utilisateur connecté
 
-<div class="item-list-tabs no-ajax" id="subnav" role="navigation">
-	<ul>
-		<?php bp_get_options_nav(); ?>
-		<?php $l_user = bp_loggedin_user_id();
-		$d_user = bp_displayed_user_id();
-		//echo 'logged'.$l_user;
-		//echo '<br>displayed'. $d_user;
-		 if ($l_user === $d_user) { ?>
-	
-	<?php } ?>
-	<?php 
-		
-		// pour tout le monde: lien vers modif Mdp
-		
-		// echo '<li id="edit-password"><a href="'.site_url().'/wp-admin/profile.php">Email & Mot de passe</a></li>';
-		
-		// pour les admin: lien vers le backend
-		if( current_user_can('administrator')) {
-			echo '<li id="edit-in-backend"><a href="'.site_url().'/wp-admin/users.php?page=bp-profile-edit&user_id='.bp_displayed_user_id().'">Edition Admin</a></li>';
-			
-			echo '<li id="edit-groups"><a href="'.site_url().'/wp-admin/user-edit.php?user_id='.bp_displayed_user_id().'#user-logement">Groupes</a></li>';
-			
-			// http://kinogeneva.4o4.ch/wp-admin/users.php?page=bp-profile-edit&user_id=243&wp_http_referer=%2Fwp-admin%2Fusers.php
-			
-			
-			//<li id="change-cover-image-personal-li"><a id="change-cover-image" href="http://kinogeneva.4o4.ch/members/rouge-cloe/profile/change-cover-image/">Modifier l'en-tête</a></li>
-		}
-	 ?>
-	</ul>
-</div><!-- .item-list-tabs -->
+if ( is_user_logged_in() ) {
 
-<?php do_action( 'bp_before_profile_content' ); ?>
+	bp_get_template_part( 'members/single/profile/subnav' );
+
+}
+
+do_action( 'bp_before_profile_content' ); ?>
 
 <div class="profile clearfix" role="main">
 
