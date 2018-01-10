60227d6541ecf053ceb8980af72b908e6447f6fe
nat <nat@etisse.ch>
2018-01-10 18:17:35 +0100

    Ticket #152 : upload de photos pour membres d'un projet: * Ajout du bouton et du formulaire d'enregistrement * modification de la fiche-projet: suppression des médias * suppression de my_kses_post() inutile (implémentée dans acf_form) A Faire: cap pour les membres d'upload de fichier

--- buddypress/groups/single/admin-add.php
+++ buddypress/groups/single/admin-add.php
@@ -1,21 +1,27 @@
 <?php
-/* Formulaire fiche projet
- * les groupes de champs ACF sont affichés sur les articles de la catégorie fiches-projet
- * les articles sont créés dans le formulaire front-end affiché ici, dans les onglets admin du groupe
- * l'association entre l'article et le groupe se fait dans une donnée meta du groupe buddypress
+
+/* ajout ACF en frontend sur les projets buddypress
+ * sources:
+ * https://www.advancedcustomfields.com/resources/acf_form/
+ * http://thestizmedia.com/front-end-posting-with-acf-pro/
+ * 
+ * Formulaire fiche projet:
+ * les groupes de champs ACF sont affichés sur les portfolios selon les réglages des groupes de champs ACF dans le backend
+ * les portfolio sont créés via le formulaire front-end (lien dans les onglets admin du groupe) lors de la première édition de la fiche-projet
+ * le titre du projet n'est pas défini par un champ ACF mais sous "gestion du projet"
+ * Il défini le titre du portfolio mais est mis à jour seulement quand on édite de la fiche projet
+ * l'association entre le portfolio et le groupe se fait via une donnée meta du groupe buddypress
  */
-	
+
 //meta info, données nécessaire à l'enregistrement
 $group_id = bp_get_group_id(); 
 $fiche_projet_post_id = groups_get_groupmeta($group_id, 'fiche-projet-post-id');
 
-//affiche le réalisateur
-global $groups_template;
-
-//id du post renseigné? On l'édite. Les champs ACF sont affichés car l'article est dans la catégorie fiches-projet 
+//id du post existe en tant que donnée meta du groupe? On édite le post.  
 if ( !empty( $fiche_projet_post_id ) ) {
 	$projet_form = array(
 		'post_id'	=> $fiche_projet_post_id,
+		'field_groups' => array(3147,3164),
 		'submit_value'	=> 'Mettre à jour la fiche projet',
 		'updated_message'    => 'Le projet a été mis à jour',
 		'html_before_fields' => '
@@ -23,7 +29,7 @@ if ( !empty( $fiche_projet_post_id ) ) {
 		<input type="hidden" name="acf[group-name]" value="'. bp_get_group_name() .'" />',
 	); 
 }
-//sinon, nouvelle fiche (=>première fois qu'on édite les détails du groupe)
+//sinon, créeation d'un post
 else {
 	$projet_form = array(
 		'post_id'		=> 'new_post',
@@ -36,8 +42,8 @@ else {
 				)
 			),
 		'field_groups' => array(3147,3164),
-		'updated_message'    => 'Le projet a été mis à jour',
-		'submit_value'		=> 'Mettre à jour la fiche projet',
+		'updated_message'    => 'Le projet a été créé',
+		'submit_value'		=> 'Créer la fiche projet',
 		'html_before_fields' => '
 		<input type="hidden" name="acf[group_id]" value="'. $group_id .'">
 		<input type="hidden" name="acf[group-name]" value="'. bp_get_group_name() .'" />',
