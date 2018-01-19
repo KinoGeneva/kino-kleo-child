a7b8573a6646b713cc13a3c0817b99d3e263a291
nat <nat@etisse.ch>
2018-01-19 14:47:09 +0100

    suppression fichier vide

--- js/kino.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($){	
-
-	// show BuddyPress Search only when page complete
-	// jQuery(".directory #buddypress > .bps_header").show();
-	// NOTE: is in members-loop.php, we apply it only there!
-
-});
