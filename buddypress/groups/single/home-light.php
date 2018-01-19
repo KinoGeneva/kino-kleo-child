242c7e9f8a7050e99304cca69836020e4062cbde
nat <nat@etisse.ch>
2018-01-19 17:18:59 +0100

    petites modifs cosmétiques

--- buddypress/groups/single/home-light.php
+++ buddypress/groups/single/home-light.php
@@ -10,6 +10,7 @@ $fiche_projet_post_id = groups_get_groupmeta($group_id, 'fiche-projet-post-id');
 	<div class="col-sm-12 projet">
 
 		<h1><?php bp_group_name(); ?></h1>
+		
 		<?php
 		if($fiche_projet_post_id){
 			echo 'Un film de <span class="strong">';
@@ -18,24 +19,18 @@ $fiche_projet_post_id = groups_get_groupmeta($group_id, 'fiche-projet-post-id');
 			the_field('duree', $fiche_projet_post_id); ?> | <?php the_field('genre', $fiche_projet_post_id); 
 			echo '</span>';
 		}
-
-		$sessions = wp_get_object_terms( $group_id , 'bp_group_tags', array('fields' => 'names') );
-		
-		echo '<h3 class="red">';
-		foreach($sessions as $session){
-			echo $session .' ';
-		}
-		echo '</h3>';
 		?>
-		
-		<div style="clear: both;"></div>
 		
 		<hr/>
 		<h3>Synopsis</h3>
+		<div>
 		<?php bp_group_description() ?>
-
-		<div style="clear: both"></div>
+		</div>
+		
+		<hr/>
 		<?php
+		$sessions = wp_get_object_terms( $group_id , 'bp_group_tags', array('fields' => 'names') );
+
 		$projections = array(
 		'Session 1 (ciné-concert) 19/01' => '19 janvier à 20h30, Ciné-concert Alhambra',
 		'Session 2 (libre) 22/01' => '22 janvier à 21h, salle centrale de la Madeleine',
